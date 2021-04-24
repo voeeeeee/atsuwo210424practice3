b0f899e7ebce53ba5c5640386e1baddeb17fe13b
Atsushi.Taketsuji <app@commude.ph>
2021-04-24 23:47:27 +0900

    Initial Commit

---
+++ practice3/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  practice3
+//
+//  Created by 竹辻篤志 on 2021/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
